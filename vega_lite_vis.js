46c9d79ef847e298af46165c44b129de07ed9bd7
YAPQIANTING <126408719+YAPQIANTING@users.noreply.github.com>
2024-10-10 10:41:51 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,47 @@
+var vg_1 = "labour_force_choropleth_map.vg.json";
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_2 = "labour_force_bar_chart_highest.vg.json";
+
+vegaEmbed("#bar_chart_highest", vg_2).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_3 = "labour_force_bar_chart_lowest.vg.json";
+
+vegaEmbed("#bar_chart_lowest", vg_3).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_5 = "labour_force_scatterplot_unemployment.vg.json";
+
+vegaEmbed("#scatterplot_unemployment", vg_5).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_6 = "bar_chart_highest_unemployment.vg.json";
+
+vegaEmbed("#bar_chart_highest_unemployment", vg_6).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_7 = "bar_chart_lowest_unemployment.vg.json";
+
+vegaEmbed("#bar_chart_lowest_unemployment", vg_7).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_8 = "donut_chart_highest.vg.json";
+
+vegaEmbed("#donut_chart_highest", vg_8).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_9 = "donut_chart_lowest.vg.json";
+
+vegaEmbed("#donut_chart_lowest", vg_9).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);

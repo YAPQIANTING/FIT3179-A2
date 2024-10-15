0a01fe573a7ae565b45b505d2cd6e0a2180276b7
YAPQIANTING <126408719+YAPQIANTING@users.noreply.github.com>
2024-10-15 23:00:44 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -21,3 +21,9 @@ var vg_4 = "/visualisations/bump_chart.vg.json";
 vegaEmbed("#bump_chart", vg_4).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_5 = "/visualisations/bump_chart_2.vg.json";
+
+vegaEmbed("#bump_chart2", vg_5).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);

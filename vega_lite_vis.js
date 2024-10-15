a212c21da9f929d25f476506c8007665436a190d
YAPQIANTING <126408719+YAPQIANTING@users.noreply.github.com>
2024-10-15 19:55:12 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,47 +1,23 @@
-var vg_1 = "labour_force_choropleth_map.vg.json";
+var vg_1 = "/visualisations/choropleth_map.vg.json";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_2 = "labour_force_bar_chart_highest.vg.json";
+var vg_2 = "/visualisations/stacked_bar_chart.vg.json";
 
-vegaEmbed("#bar_chart_highest", vg_2).then(function(result) {
+vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_3 = "labour_force_bar_chart_lowest.vg.json";
+var vg_3 = "/visualisations/trellis_area_chart.vg.json";
 
-vegaEmbed("#bar_chart_lowest", vg_3).then(function(result) {
+vegaEmbed("#trellis_area_chart", vg_3).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_5 = "labour_force_scatterplot_unemployment.vg.json";
+var vg_4 = "/visualisations/bump_chart.vg.json";
 
-vegaEmbed("#scatterplot_unemployment", vg_5).then(function(result) {
-  // Access the Vega view instance as result.view
-}).catch(console.error);
-
-var vg_6 = "bar_chart_highest_unemployment.vg.json";
-
-vegaEmbed("#bar_chart_highest_unemployment", vg_6).then(function(result) {
-  // Access the Vega view instance as result.view
-}).catch(console.error);
-
-var vg_7 = "bar_chart_lowest_unemployment.vg.json";
-
-vegaEmbed("#bar_chart_lowest_unemployment", vg_7).then(function(result) {
-  // Access the Vega view instance as result.view
-}).catch(console.error);
-
-var vg_8 = "donut_chart_highest.vg.json";
-
-vegaEmbed("#donut_chart_highest", vg_8).then(function(result) {
-  // Access the Vega view instance as result.view
-}).catch(console.error);
-
-var vg_9 = "donut_chart_lowest.vg.json";
-
-vegaEmbed("#donut_chart_lowest", vg_9).then(function(result) {
+vegaEmbed("#bump_chart", vg_4).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
